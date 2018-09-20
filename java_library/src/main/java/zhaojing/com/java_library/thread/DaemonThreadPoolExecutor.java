067e560512541d1de40b7d42d8eafe024d222943
gmtalang <zhaojing522521@163.com>
2018-09-20 14:02:06 +0800

    use threadFactory

---
+++ java_library/src/main/java/zhaojing/com/java_library/thread/DaemonThreadPoolExecutor.java
@@ -0,0 +1,18 @@
+package zhaojing.com.java_library.thread;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * AUTHOR：Created by Administrator on 2018-09-20 12:51
+ * EMAIL:  2910763715@qq.com
+ */
+
+public class DaemonThreadPoolExecutor extends ThreadPoolExecutor {
+    public DaemonThreadPoolExecutor() {
+        super(0, Integer.MAX_VALUE, 60L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>(), new DaemonThreadFactory());
+    }
+}
